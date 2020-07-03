136c854c5eb31c74f4fad5af2190b0459f6e0791
dilipkumarm219@gmail.com <dilipkumarm219@gmail.com>
2020-07-03 19:44:37 +0530

    feat: 	-- user controller added with index 	-- user sign_in added in layouts/application.html.erb 	-- added index for places and user model 	-- added routes for users index action 	-- added the title for places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_192843) do
+ActiveRecord::Schema.define(version: 2020_07_03_025253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_192843) do
     t.integer "share_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "shares", force: :cascade do |t|
