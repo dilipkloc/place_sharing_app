b38e61005f559f5cd7f2d42aba36412d45f8e8e5
dilipkumarm219@gmail.com <dilipkumarm219@gmail.com>
2020-07-04 09:22:57 +0530

    feat: 	-- added show option under user index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: 'user'
 
   root 'about#index'
 end
